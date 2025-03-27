c99602c908f1bb2bb4b28e5436cf11f6386bad36
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-27 11:33:33 +0200

    Edit: added MyGraph functions - addVertice, addEdge, getIndexOfVertice

--- src/datastr/MyGraph.java
+++ src/datastr/MyGraph.java
@@ -59,5 +59,92 @@ public class MyGraph <Ttype>{
 	}
 	
 	
+	public void addVertice(Ttype element) throws Exception
+	{
+		if(element == null)
+		{
+			throw new Exception("Element can not be null");
+		}
+		
+		if(isFull())
+		{
+			resize();
+		}
+		
+		MyVerticeNode newVerticeNode = new MyVerticeNode(element);
+		vertices[counter++] = newVerticeNode;
+		
+		
+	}
+	
+	public void addEdge(Ttype elementFrom, Ttype elementTo, float weight) throws Exception {
+		if(elementFrom == null || elementTo == null || weight <= 0) {
+			throw new Exception("Wrong input params");
+		}
+		
+		int indexOfElementFrom = getIndexOfVertice(elementFrom);
+		int indexOfElementTo = getIndexOfVertice(elementTo);
+		
+		if(indexOfElementFrom == -1)
+		{
+			throw new Exception("Element from (" + elementFrom + ") doesn't exist");
+		}
+		
+		if(indexOfElementTo == -1)
+		{
+			throw new Exception("Element to (" + elementTo + ") doesn't exist");
+		}
+		
+		//pārbaudīsim, vai jau savienojuma mezgls neeksistē
+		
+		MyEdgeNode tempEdge = vertices[indexOfElementFrom].getFirstEdgeNode();
+		
+		while(tempEdge != null)
+		{
+			if(tempEdge.getIndexOfVerticeEdgeTo() == indexOfElementTo
+					&& tempEdge.getWeight() == weight)
+			{
+				throw new Exception(
+				"The edge between " + elementFrom + "->" + elementTo +  " already exists");
+			}
+				
+			
+			tempEdge = tempEdge.getNext();
+		}
+		
+		MyEdgeNode newEdge = new MyEdgeNode(indexOfElementTo, weight);
+		//savienojuma mezgls būs jāieliek kā pirmais
+		if(vertices[indexOfElementFrom].getFirstEdgeNode() == null)
+		{
+			vertices[indexOfElementFrom].setFirstEdgeNode(newEdge);
+		}
+		else // pie virsotnes ir jau pieasitīts vismaz viens savienojumu mezgls
+		{
+			MyEdgeNode currentFirstEdge = vertices[indexOfElementFrom].getFirstEdgeNode();
+			newEdge.setNext(currentFirstEdge);
+			currentFirstEdge.setPrevious(newEdge);
+			vertices[indexOfElementFrom].setFirstEdgeNode(newEdge);		
+			
+		}
+		
+		
+		
+	}
+	
+	private int getIndexOfVertice(Ttype element) {
+		//TODO pēc nepieciešamšības pārbaudam not null
+		
+		for(int i = 0 ; i < counter; i++) {
+			if(vertices[i].getVerticeElement().equals(element))
+			{
+				return i;
+			}
+		}
+		
+		return -1;
+		
+		
+	}
+	
 
 }
