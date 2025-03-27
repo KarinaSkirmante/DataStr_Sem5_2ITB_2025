88a0396a830f91a4d5ad966d0618ac8b8e2bee84
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-27 10:27:46 +0200

    Edit: added variables, gets, sets, arg-constructor and toString in MyedgeNode

--- src/datastr/MyEdgeNode.java
+++ src/datastr/MyEdgeNode.java
@@ -1,5 +1,60 @@
 package datastr;
 
 public class MyEdgeNode {
+	private int indexOfVerticeEdgeTo;
+	private float weight;//TODO var uztaisīt kā Ttype, kas ļautu ielikt arī saliktus datus, piemēram, ceļa garums + max ātrums + stāvoklis
+	private MyEdgeNode next = null;
+	private MyEdgeNode previous = null;
+	
+	
+	public int getIndexOfVerticeEdgeTo() {
+		return indexOfVerticeEdgeTo;
+	}
+	public void setIndexOfVerticeEdgeTo(int indexOfVerticeEdgeTo) {
+		if(indexOfVerticeEdgeTo >= 0)
+		{
+			this.indexOfVerticeEdgeTo = indexOfVerticeEdgeTo;
+		}
+		else
+		{
+			this.indexOfVerticeEdgeTo = 0;//TODO var apdomāt uz kādu citu vērtibu
+		}
+	}
+	public float getWeight() {
+		return weight;
+	}
+	public void setWeight(float weight) {
+		if(weight > 0 )
+		{
+			this.weight = weight;
+		}
+		else
+		{
+			this.weight = 0.0001f;
+		}
+	}
+	public MyEdgeNode getNext() {
+		return next;
+	}
+	public void setNext(MyEdgeNode next) {
+		this.next = next;
+	}
+	public MyEdgeNode getPrevious() {
+		return previous;
+	}
+	public void setPrevious(MyEdgeNode previous) {
+		this.previous = previous;
+	}
+	
+	public MyEdgeNode(int indexOfVerticeEdgeTo, float weight)
+	{
+		setIndexOfVerticeEdgeTo(indexOfVerticeEdgeTo);
+		setWeight(weight);
+	}
+	
+	
+	public String toString() {
+		return "[" + indexOfVerticeEdgeTo + "] " + weight;
+	}
 
 }
